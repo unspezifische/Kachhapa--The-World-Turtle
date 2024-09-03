d980ebc2dc892772cc15e65ceb912f7abdbb42ea
Ian Johnson <unspezifische@gmail.com>
2024-09-03 16:55:12 -0500

    first successful Docker build- might contain connection issues

--- webapp/src/Login.js
+++ webapp/src/Login.js
@@ -5,7 +5,6 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios'; // Import axios
 
 import './Login.css';
-import { set } from 'jodit/esm/core/helpers';
 
 function Login({ setIsLoggedIn, setToken, setUserID, setIsLoading, setAppUsername }) {
   const [username, setUsername] = useState('');
