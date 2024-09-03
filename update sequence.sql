5ad365029eecc37ef6f98a66d309fe52883f2de5
Ian Johnson <unspezifische@gmail.com>
2024-09-03 17:25:08 -0500

    project structure changes

--- update sequence.sql
+++
@@ -1,2 +0,0 @@
-SELECT pg_get_serial_sequence('inventory', 'id');
-SELECT setval('public.inventory_id_seq', (SELECT COALESCE(MAX(id), 1) FROM inventory));
--- webapp/toDev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This script prepends "http://127.0.0.1:5001" to all "/api/*" occurrences in .js files in the src directory
-
-find ./src -name "*.js" -exec sed -i '' 's|/api/|http://127.0.0.1:5001/api/|g' {} \;
